cf72e71fce1ee8e2a67f9881c1fbc1085302a296
itouchzh <tootatoo666@gmail.com>
2024-02-14 16:18:34 +0800

    chore: 增加CCTSDB数据集

--- utils/loss.py
+++ utils/loss.py
@@ -179,14 +179,13 @@ class ComputeLoss:
                 # +++++++++++++++++++++++++++
                 #
                 nwd = wasserstein_loss(pbox, tbox[i]).squeeze()
-                # iou_ratio = 1.0
+                iou_ratio = 1.0
                 # iou_ratio = 0.3
                 # iou_ratio = 0
                 # iou_ratio = 0.6
                 # iou_ratio = 0.7
-                iou_ratio = 0.5
+                # iou_ratio = 0.5
                 lbox += (1 - iou_ratio) * (1.0 - nwd).mean() + iou_ratio * (1.0 - iou).mean()  # iou loss
-
                 # Objectness
                 iou = (iou.detach() * iou_ratio + nwd.detach() * (1 - iou_ratio)).clamp(0, 1).type(tobj.dtype)
                 # iou = (iou.detach().* iou_ratio.clamp(0).type(tobj.dtype)  + nwd.detach() * (1 - iou_ratio)).clamp(0, 1).type(tobj.dtype)
